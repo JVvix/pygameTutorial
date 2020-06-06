784d6b409a0184d3b456465891d9ad291ff82376
JVvix <fayedotly@gmail.com>
2020-06-06 16:38:10 -0700

    almost finished with #3

--- tutorial_one.py
+++ tutorial_one.py
@@ -7,10 +7,17 @@ win = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("First Game")
 
+img = pygame.image.load()
+
+walkRight = [img('R1.png'), img('R2.png'),img('R3.png'),img('R4.png'),img('R5.png'),img('R6.png'),img('R7.png'),img('R8.png'),img('R9.png')]
+walkLeft = [img('L1.png'), img('L2.png'),img('L3.png'),img('L4.png'),img('L5.png'),img('L6.png'),img('L7.png'),img('L8.png'),img('L9.png')]
+bg = img('bg.png')
+char = img('standing.png')
+
 x = 50
 y = 440 
-width = 40
-height = 60
+width = 64
+height = 64
 vel = 5
 
 isJump = False
@@ -18,6 +25,17 @@ jumpCount = 10
 left = False
 right = False
 walkCount = 0
+
+def reddrawGameWindow():
+    global walkCount
+
+    win.blit(bg, (0, 0))
+    if walkCount + 1 >= 27:
+        walkCount = 0 
+
+    if left:
+        win.blit(walkLeft[walkCount])
+    pygame.display.update()
 
 run = True
 while run:
@@ -31,15 +49,36 @@ while run:
 
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < 500 - width - vel:
         x += vel
+        right = True
+        left = False
+    else:
+        right = False
+        left = False
+        walkCount = 0
+
     if keys[pygame.K_a] and x > vel:
         x -= vel
-    if keys[pygame.K_d] and x < 500 - width - vel:
+        left = True
+        right = False
+    elif keys[pygame.K_d] and x < 500 - width - vel:
         x += vel
+        right = True
+        left = False
+    else:
+        right = False
+        left = False
+        walkCount = 0
+        
     if not (isJump):
         if keys[pygame.K_SPACE]:
             isJump = True
+            right = False
+            left = False
+            walkCount = 0
     else:
         if jumpCount >= -10 and y > vel:
             neg = 1
@@ -52,9 +91,6 @@ while run:
             jumpCount = 10
     if keys[pygame.K_q]:
        event.type = pygame.QUIT
-
-    win.fill((0,0,0))
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
+    redrawGameWindow
 
 pygame.quit() 
